528b8acdbdad081efe0f6d3f474fa38ec64fe5dd
Jan Rose <janrose1987@gmail.com>
2020-06-30 15:52:12 -0400

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,4 +21,23 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+  	@portfolio_item = Portfolio.find(params[:id])
+  end
+
+  # PATCH/PUT /portfolios/1
+  # PATCH/PUT /portfolios/1.json
+  def update
+  	@portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+
 end
